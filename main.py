0e119a1a4c63882a73aeac34b1db90505f1c1eb8
Mario Lasić <mario.lasic@outlook.com>
2025-03-10 15:53:31 +0100

    Add XOX game class with initial UI setup

--- main.py
+++ main.py
@@ -2,3 +2,30 @@ from PySide6.QtWidgets import (QApplication, QWidget,
                                QPushButton, QLabel, QGridLayout)
 import sys
 
+class Xox(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('XOX')
+        self.setGeometry(100, 100, 400, 400)
+
+        self.createUI()
+
+    
+    def createUI(self):
+        self.signs = [['', '', ''], ['', '', ''], ['', '', '']]
+        self.current_player = 'X'
+
+        layout = QGridLayout()
+        for row in range(3):
+            for column in range(3):
+                button = QPushButton()
+                button.setProperty('classes', 'row column')
+                button.clicked.connect(self.button_clicked)
+                layout.addWidget(button, row, column)
+        
+        self.setLayout(layout)
+
+    def button_clicked(self):
+        pass
+    
